26d601f21a28eaa00afbc8679bc93e070dbe7420
IvanSmirnov <>
2025-08-26 23:06:44 +0500

    Добавление сущности Topic

---
+++ Domain/Models/Topic.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Abstractions;
+using Domain.ValueObjects;
+
+namespace Domain.Models;
+
+public class Topic : Entity<TopicId>
+{
+    public string Title { get; set; } = default!;
+    public DateTime? EventStart { get; set; }
+    public string Summary { get; set; } = default!;
+    public string TopicType { get; set; } = default!;
+    public Location Location { get; set; } = default!;
+
+    public static Topic Create(
+        TopicId id,
+        string title,
+        DateTime eventStart,
+        string summary,
+        string topicType,
+        Location location)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(title);
+        ArgumentException.ThrowIfNullOrWhiteSpace(summary);
+        ArgumentException.ThrowIfNullOrWhiteSpace(topicType);
+
+        Topic topic = new Topic()
+        {
+            Id = id,
+            Title = title,
+            EventStart = eventStart,
+            Summary = summary,
+            TopicType = topicType,
+            Location = location
+        };
+
+        return topic;
+    }
+}

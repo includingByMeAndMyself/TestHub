3c6afb27046a6613fd668c287972982df645c204
IvanSmirnov <>
2025-08-27 20:04:06 +0500

    Добавление DbContext + выполнение первой миграции

---
+++ Application/Data/DataBaseContext/IApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Data.DataBaseContext;
+
+public interface IApplicationDbContext
+{
+    DbSet<Topic> Topics { get; }
+}
---
+++ Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Application.Data.DataBaseContext;
+using Domain.Models;
+using Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.DataBaseContext;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public DbSet<Topic> Topics => Set<Topic>();
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Topic>()
+            .Property(topic => topic.Id)
+            .HasConversion(
+                id => id.Value,
+                value => TopicId.Of(value));
+
+        modelBuilder.Entity<Topic>()
+            .OwnsOne(topic => topic.Location, location =>
+            {
+                location.Property(l => l.City).HasColumnName("City");
+                location.Property(l => l.Street).HasColumnName("Street");
+            });
+    }
+}

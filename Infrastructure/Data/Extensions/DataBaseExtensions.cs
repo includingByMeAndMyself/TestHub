e69b5ab444d0e0f31484808555f0aded1c1b9554
IvanSmirnov <>
2025-08-27 20:40:01 +0500

    Добавил автоинициализацию значениями при старте приложения

---
+++ Infrastructure/Data/Extensions/DataBaseExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Data.DataBaseContext;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Data.Extensions;
+
+public static class DataBaseExtensions
+{
+    public static async Task InitializeDatabaseAsync(this WebApplication app)
+    {
+        using IServiceScope scope = app.Services.CreateScope();
+        var dbContext = scope
+            .ServiceProvider
+            .GetRequiredService<ApplicationDbContext>();
+
+        await dbContext.Database.MigrateAsync();
+
+        await SeedData(dbContext);
+    }
+
+    private static async Task SeedData(ApplicationDbContext dbContext)
+    {
+        await SeedTopicsAsync(dbContext);
+    }
+
+    private static async Task SeedTopicsAsync(ApplicationDbContext dbContext)
+    {
+        if (!await dbContext.Topics.AnyAsync())
+        {
+            await dbContext.Topics.AddRangeAsync(InitialData.Topics);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
